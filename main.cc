625159b26965a0ba0ea03a91c6a002bb2bacd33d
moon1523 <moonsungho@hanyang.ac.kr>
2021-10-28 21:49:46 +0900

    updated on 2021-10-28

---
+++ main.cc
@@ -0,0 +1,97 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file main.cc
+/// \brief G4Template code
+
+#include "DetectorConstruction.hh"
+#include "ActionInitialization.hh"
+
+#include "G4RunManagerFactory.hh"
+
+#include "G4UImanager.hh"
+#include "FTFP_BERT.hh"
+
+#include "G4VisExecutive.hh"
+#include "G4UIExecutive.hh"
+
+#include "Randomize.hh"
+
+int main(int argc,char** argv)
+{
+	G4UIExecutive* ui = 0;
+	if ( argc == 1 ) {
+		ui = new G4UIExecutive(argc, argv);
+	}
+
+	auto* runManager = G4RunManagerFactory::CreateRunManager(G4RunManagerType::MT);
+
+	// Choose the Random engine
+	G4Random::setTheEngine(new CLHEP::RanecuEngine);
+	G4Random::setTheSeed(time(NULL));
+
+	// Set mandatory initialization classes
+	//
+	// Detector construction
+	runManager->SetUserInitialization(new DetectorConstruction());
+
+	// Physics list
+	G4VModularPhysicsList* physicsList = new FTFP_BERT;
+	physicsList->SetVerboseLevel(1);
+	runManager->SetUserInitialization(physicsList);
+
+	// User action initialization
+	runManager->SetUserInitialization(new ActionInitialization());
+
+	// Initialize visualization
+	//
+	G4VisManager* visManager = new G4VisExecutive;
+	visManager->Initialize();
+
+	// Get the pointer to the User Interface manager
+	G4UImanager* UImanager = G4UImanager::GetUIpointer();
+
+	// Process macro or start UI session
+	//
+	if ( ! ui ) {
+		// batch mode
+		G4String command = "/control/execute ";
+		G4String fileName = argv[1];
+		UImanager->ApplyCommand(command+fileName);
+	}
+	else {
+		// interactive mode
+		UImanager->ApplyCommand("/control/execute init_vis.mac");
+		ui->SessionStart();
+		delete ui;
+	}
+
+	delete visManager;
+	delete runManager;
+
+	return EXIT_SUCCESS;
+}
+
---
+++ src/DetectorConstruction.cc
@@ -0,0 +1,78 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+//
+/// \file DetectorConstruction.cc
+/// \brief Implementation of the DetectorConstruction class
+
+#include "DetectorConstruction.hh"
+
+#include "G4RunManager.hh"
+#include "G4NistManager.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PVPlacement.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4VisAttributes.hh"
+
+#include "G4Box.hh"
+
+
+
+DetectorConstruction::DetectorConstruction()
+: G4VUserDetectorConstruction(),
+  lv_world(0), pv_world(0)
+{ }
+
+
+
+DetectorConstruction::~DetectorConstruction()
+{ }
+
+
+
+G4VPhysicalVolume* DetectorConstruction::Construct()
+{
+	SetupWorldGeometry();
+
+
+  return pv_world;
+}
+
+void DetectorConstruction::SetupWorldGeometry()
+{
+	// Define the world box (size: 10*10*5 m3)
+	G4double world_halfX = 5. * m;
+	G4double world_halfY = 5. * m;
+	G4double world_halfZ = 2.5 * m;
+
+	G4VSolid* sol_world = new G4Box("sol_world", world_halfX, world_halfY, world_halfZ);
+	lv_world = new G4LogicalVolume(sol_world, G4NistManager::Instance()->FindOrBuildMaterial("G4_AIR"), "lv_world");
+	pv_world = new G4PVPlacement(0, G4ThreeVector(), lv_world, "pv_world", 0, false, 0, false);
+	G4VisAttributes* va_world = new G4VisAttributes(G4Colour(1.0,1.0,1.0));
+	va_world->SetForceWireframe(true);
+	lv_world->SetVisAttributes(va_world);
+}
+
+
